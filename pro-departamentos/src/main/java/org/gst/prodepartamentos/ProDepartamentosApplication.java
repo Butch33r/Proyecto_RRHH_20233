1134377940d8e0824ccf3264c504b06cd2e4b9a3
S3 <24_de_7@protonmail.com>
2023-02-11 00:46:35 -0500

    Configuración del microservicio Departamento

---
+++ pro-departamentos/src/main/java/org/gst/prodepartamentos/ProDepartamentosApplication.java
@@ -0,0 +1,13 @@
+package org.gst.prodepartamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProDepartamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProDepartamentosApplication.class, args);
+	}
+
+}
